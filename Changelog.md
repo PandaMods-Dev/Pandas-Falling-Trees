9cd07fa4c4d171c593fe5cb9046e6ccb37e39984
PandaDap2006 <Oliver.h.froberg@gmail.com>
2023-06-27 19:54:34 +0200

    Added Changelog.md

---
+++ Changelog.md
@@ -0,0 +1,7 @@
+<h2 style="text-align: center;">Version 0.3 Changelog</h2>
+- Remade the Tree detection Algorithm.
+- Made automatic log detection by checking if block is part of Log BlockTag.
+- Change leaves detection to if block is part of Leaves BlockTag.
+- Added Food Exhaustion for each log of the tree.
+- Added Block mined stat for each block of the tree.
+- Added one block mining when shifting.
--- common/src/main/java/me/pandadev/fallingtrees/mixin/BlockMixin.java
+++ common/src/main/java/me/pandadev/fallingtrees/mixin/BlockMixin.java
@@ -58,81 +58,14 @@ public abstract class BlockMixin {
 				if (usedItem.isDamageableItem()) {
 					usedItem.hurtAndBreak(LogAmount-1, player, player1 -> {});
 				}
-				player.awardStat(Stats.BLOCK_MINED.get((Block) (Object)this));
-        		player.causeFoodExhaustion(0.005F*LogAmount);
+
+				player.causeFoodExhaustion(0.005F*LogAmount);
 
 				for (Map.Entry<BlockPos, BlockState> entry : treeBlocks.entrySet()) {
+					player.awardStat(Stats.BLOCK_MINED.get(entry.getValue().getBlock()));
 					level.setBlockAndUpdate(entry.getKey().offset(blockPos), Blocks.AIR.defaultBlockState());
 				}
 			}
 		}
 	}
-
-//	@Inject(method = "playerWillDestroy",
-//			at = @At(
-//					value = "INVOKE",
-//					target = "Lnet/minecraft/world/level/Level;gameEvent(Lnet/minecraft/world/level/gameevent/GameEvent;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/gameevent/GameEvent$Context;)V"
-//			),
-//			cancellable = true)
-//	public void destroy(Level level, BlockPos pos, BlockState state, Player player, CallbackInfo ci) {
-//		if (TreesConfig.isValidLog(state.getBlock())) {
-//			Map<BlockPos, BlockState> blocks = new HashMap<>();
-//
-//			addTreeBlock(level, pos, pos, pos, blocks, state.getBlock());
-//
-//			if (blocks.values().stream().anyMatch(state1 -> state1.getBlock() instanceof LeavesBlock && !state1.getValue(LeavesBlock.PERSISTENT))) {
-//
-//				TreeEntity treeEntity = new TreeEntity(FallingTrees.TREE_ENTITY.get(), level).setBlocks(blocks);
-//				Vector3d position = new Vector3d(pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5);
-//				treeEntity.setPos(position.x, position.y, position.z);
-//				ItemStack usedItem = player.getMainHandItem();
-//				treeEntity.usedItem = usedItem;
-//				treeEntity.setRotationY((float) Math.atan2(player.getX() - position.x, player.getZ() - position.z));
-//				level.addFreshEntity(treeEntity);
-//
-//				int LogAmount = (int) blocks.values().stream().filter(state1 -> !(state1.getBlock() instanceof LeavesBlock)).count();
-//				if (usedItem.isDamageableItem()) {
-//					usedItem.hurtAndBreak(LogAmount, player, player1 -> {});
-//				}
-//
-//				ci.cancel();
-//				for (Map.Entry<BlockPos, BlockState> entry : blocks.entrySet()) {
-//					level.removeBlock(new BlockPos(
-//							entry.getKey().getX() + pos.getX(),
-//							entry.getKey().getY() + pos.getY(),
-//							entry.getKey().getZ() + pos.getZ()
-//					), false);
-//				}
-//			}
-//		}
-//	}
-
-//	private void addTreeBlock(Level level, BlockPos pos, BlockPos basePos, BlockPos lastLog, Map<BlockPos, BlockState> blocks, Block log) {
-//        BlockState state = level.getBlockState(pos);
-//        Block block = state.getBlock();
-//
-//		boolean isLog = state.is(log);
-//		boolean isLeave = block instanceof LeavesBlock;
-//		int leaveDistance;
-//
-//        if (isLog || isLeave) {
-//			if (isLeave) {
-//				leaveDistance = state.getValue(LeavesBlock.DISTANCE);
-//				if (lastLog.distManhattan(pos) > leaveDistance + 1)
-//					return;
-//			}
-//			if (isLog) {
-//				if (lastLog.distManhattan(pos) > 1 && isLogConnectedToGround(level, pos))
-//					return;
-//				lastLog = pos;
-//			}
-//
-//			blocks.put(pos.subtract(basePos), state);
-//
-//            for (BlockPos neighborPos : BlockPos.betweenClosed(pos.offset(-1, 0, -1), pos.offset(1, 1, 1))) {
-//				if (!blocks.containsKey(neighborPos.subtract(basePos)))
-//					addTreeBlock(level, neighborPos, basePos, lastLog, blocks, log);
-//            }
-//        }
-//    }
 }

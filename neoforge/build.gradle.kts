1e302d88734cb35f8a30f58b416c5be30b64acc6
The Panda Oliver <Oliver.h.froberg@gmail.com>
2024-11-06 23:40:59 +0100

    Introduce ShadowJar handling for RemapJarTask
    Import ShadowJar and set input files for RemapJarTask in both fabric and neoforge build scripts. This ensures that the ShadowJar output is used correctly during the remapping process.

--- neoforge/build.gradle.kts
+++ neoforge/build.gradle.kts
@@ -1,3 +1,6 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import net.fabricmc.loom.task.RemapJarTask
+
 architectury {
 	platformSetupLoomIde()
 	neoForge()
@@ -27,4 +30,9 @@ dependencies {
 tasks.remapJar {
 	injectAccessWidener = true
 	atAccessWideners.add(loom.accessWidenerPath.get().asFile.name)
+}
+
+tasks.withType<RemapJarTask> {
+	val shadowJar = tasks.getByName<ShadowJar>("shadowJar")
+	inputFile.set(shadowJar.archiveFile)
 }

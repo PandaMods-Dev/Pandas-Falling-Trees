74553c448451576f514016f686a5c03cf01aa471
The Panda Oliver <Oliver.h.froberg@gmail.com>
2024-11-21 16:27:25 +0100

    Fix mixin config naming and set JAVA compatibility level
    Corrected mixin configuration names in neoforge.mods.toml from 'modId' to 'mod_id' for consistency. Updated the 'compatibilityLevel' in fallingtrees.mixins.json to 'JAVA_21'. Commented out unused dependencies in fabric/build.gradle.kts.

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -27,8 +27,8 @@ dependencies {
 	modApi("com.terraformersmc:modmenu:${properties["deps_modmenu_version"]}")
 
 	modCompileOnly("maven.modrinth:treechop:${properties["deps_ht_treechop_version"]}-fabric,${properties["deps_ht_treechop_mc_version"]}")
-	modRuntimeOnly("maven.modrinth:treechop:${properties["deps_ht_treechop_version"]}-fabric,${properties["deps_ht_treechop_mc_version"]}")
-	modLocalRuntime("fuzs.forgeconfigapiport:forgeconfigapiport-fabric:${properties["deps_forge_config_api_version"]}")
+//	modRuntimeOnly("maven.modrinth:treechop:${properties["deps_ht_treechop_version"]}-fabric,${properties["deps_ht_treechop_mc_version"]}")
+//	modLocalRuntime("fuzs.forgeconfigapiport:forgeconfigapiport-fabric:${properties["deps_forge_config_api_version"]}")
 
 	modCompileOnly("maven.modrinth:jade:${properties["deps_jade_version"]}+fabric-fabric,${properties["deps_jade_mc_version"]}")
 //	modLocalRuntime("maven.modrinth:jade:${properties["deps_jade_version"]}+fabric-fabric,${properties["deps_jade_mc_version"]}")
